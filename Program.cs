4da93426896d37f6850cd1e4631dc0555e16be62
VolfoXxx <118421435+VolfoXxx@users.noreply.github.com>
2022-11-21 16:53:40 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using library;
+
+namespace _301.Практическая_2
+{
+    class User
+    {
+        public string login = "login", password = "pass", second_name = "Smith", first_name = "John";
+
+        public void Authorize()
+        {
+            Console.WriteLine("Enter your login");
+            string login1 = Console.ReadLine();
+
+            Console.WriteLine("Enter your password");
+            string password1 = Console.ReadLine();
+
+            if (login == login1 && password == password1)
+            {
+                Console.WriteLine($"Your name is - {first_name}\nYour second name is - {second_name}.");
+                Program program = new Program();
+                Person per = new Person();
+                per.Greeting(first_name);
+                program.Middle();
+            }
+            else
+            {
+                Console.WriteLine("Your password or login is wrong. Please enter your password and login again");
+                Authorize2();
+            }
+        }
+        private void Authorize2()
+        {
+            Authorize();
+        }
+    }
+
+    class Program
+    {
+        static int a, b;
+
+        static void Begin()
+        {
+            Console.WriteLine("Enter 1(+), 2(-), 3(*) or 4(/)");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (c == 1)
+            {
+                Console.WriteLine("a minus b equals to - " + Plus(a, b));
+            }
+            else if (c == 2)
+            {
+                Console.WriteLine("a minus b equals to - " + Minus(a, b));
+            }
+            else if (c == 3)
+            {
+                Person per = new Person();
+                per.Multiply(a, b);
+                
+            }
+            else if (c == 4)
+            {
+                Person per = new Person();
+                per.Division(a, b);
+            }
+            else
+            {
+                Console.WriteLine("You entered wrong character, please enter 1(+), 2(-), 3(*) or 4(/)");
+                End();
+            }
+        }
+
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void End() 
+        {
+            Begin();
+        }
+
+        public void Middle()
+        {
+            Console.WriteLine($"Enter the number - ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Enter the number - ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Begin();
+        }
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Authorize();
+        }
+    }
+}
