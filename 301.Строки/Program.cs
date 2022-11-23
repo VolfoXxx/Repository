2fa5ee8dfaa9c7e6784c711e28dfad726df3abb8
VolfoXxx <118421435+VolfoXxx@users.noreply.github.com>
2022-11-23 16:17:40 +0900

    Add files via upload

---
+++ 301.Строки/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _301.Строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogname = "name";
+            string ogsur = "surname";
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите вашу фамилию");
+            string sur = Console.ReadLine();
+
+            char m = 'm';
+            int surint = sur.IndexOf(m);
+
+            bool name1 = string.Equals(ogname, name);
+
+            int local1 = string.CompareOrdinal(ogname, name);
+            int local2 = string.CompareOrdinal(ogsur, sur);
+            if(local1<0)
+            {
+                Console.WriteLine("Выбрана неверная раскладка клавиатуры");
+            }
+            else
+            if(local2<0)
+            {
+                Console.WriteLine("Выбрана неверная раскладка клавиатуры");
+            }
+            else
+            if (!name1)
+            {
+                Console.WriteLine("Введено неверное имя");
+            }
+            else
+            if (!sur.Contains("surname"))
+            {
+                Console.WriteLine("Введена неверная фамилия");
+            }
+            else
+            if (surint == 5) 
+            {
+                Console.WriteLine("Введена верная фамилия");
+            }
+
+            /*int compresname = string.CompareOrdinal(name, ogname);
+            int compressur = string.CompareOrdinal(sur, ogsur);
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 301.Строки/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("301.Строки")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("301.Строки")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fac854de-4a00-4a67-bf84-56f4e53485af")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
